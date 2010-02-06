13fa2845a02a63d41a297733c3e93fb90a6f39b4
Walter Da Col <walter.dacol@gmail.com>
2010-02-06 17:32:20 +0100

    Fix Launch.sh (Qemu)

--- launch.sh
+++ launch.sh
@@ -1,4 +1,7 @@
 #!bin/sh
+QEMU="qemu-kvm"
+which "${QEMU}" &>/dev/null || QEMU="qemu"
+
 pkill qemu
 cp Module/mydev.ko Vm/ramfs/
 cp Writer/writer Vm/ramfs/
@@ -6,4 +9,5 @@ cp Reader/reader Vm/ramfs/
 cd Vm/ramfs
 sh mkinitramfs.sh ../tmpfs
 cd -
-qemu-kvm -kernel Vm/linux-2.6.19/bzImage -initrd Vm/tmpfs -hda /dev/null -m 128&
+${QEMU} -kernel Vm/linux-2.6.19/bzImage -initrd Vm/tmpfs -hda /dev/null -m 128&
+
